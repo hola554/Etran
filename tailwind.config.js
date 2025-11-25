66a67c9b2f393457be5192d95897042a452a4977
Lawal Baseet <lawalbaseet2020@gmail.com>
2025-11-25 12:24:10 +0100

    Committed all pending changes before testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./src/**/*.{ts,tsx,js,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        limeBright: {
+          light: '#a3d977',
+          DEFAULT: '#b8e986',
+        },
+        mediumGreen: '#7a9b2e',
+        darkOliveGreen: '#3d4a26',
+        offWhiteCream: {
+          light: '#f8f8f6',
+          DEFAULT: '#fafaf9',
+        },
+        lightGray: '#e5e5e3',
+        mediumGray: '#888888',
+        darkCharcoal: '#333333',
+        veryDarkGray: '#1c1c1c',
+        darkGreenGray: '#323d2a',
+      }
+    },
   },
   plugins: [],
 }
